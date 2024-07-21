a3ed34792bee14e7ce838061eb6934b38ec9ceb9
Shegsdev <akanbisegun1@gmail.com>
2024-07-21 15:51:27 +0100

    initialize game class

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+	"""Manage game assets and behaviour"""
+
+	def __init__(self):
+		pygame.init()
+
+		self.screen = pygame.display_set_mode((1200, 800))
+		pygame.display_set_caption("Alien Invasion")
+
+	def run_game(self):
+		"""Start game loop"""
+		while True:
+			# watch for keyboard and mouse events
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+
+			# display drawn screen
+			pygame.display.flip()
+
+
+if __name__ == "__main__":
+	ai = AlienInvasion()
+	ai.run_game()
